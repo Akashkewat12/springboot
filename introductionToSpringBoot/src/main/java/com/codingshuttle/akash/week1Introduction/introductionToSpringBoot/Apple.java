3732b1c6e125cd12514a3b121e0625e105f3fcef
Akashkewat12 <akashbind.bca.15@gmail.com>
2025-01-06 18:32:29 +0530

    springboot

---
+++ introductionToSpringBoot/src/main/java/com/codingshuttle/akash/week1Introduction/introductionToSpringBoot/Apple.java
@@ -0,0 +1,10 @@
+package com.codingshuttle.akash.week1Introduction.introductionToSpringBoot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Apple {
+    void eatApple() {
+        System.out.println("I am eating the apple.");
+    }
+}
